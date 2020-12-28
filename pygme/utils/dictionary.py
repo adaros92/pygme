301a7136ba58684a922eb0cdc9cc2e53a1d38bdf
adamsrosales <adams.rosales.92@gmail.com>
2020-12-28 15:16:26 -0800

    Implement word class in dictionary utils

--- pygme/utils/dictionary.py
+++ pygme/utils/dictionary.py
@@ -4,8 +4,84 @@ import random
 import sys
 
 
-class Dictionary(object):
+class Word(object):
+    """ Represents a word in a dictionary. Used to provide supplements to existing str class by not taking into
+    account capitalization when comparing words and hiding certain characters when returning the word to a caller.
 
+    Constructor parameters:
+
+    @param word - a regular string word
+    @param show_only - an optional set containing characters to exclude from the word when repr or str are called
+    """
+
+    def __init__(self, word: str, show_only: set = None):
+        super().__init__()
+        if " " in word:
+            raise ValueError("The given word {0} contains whitespace".format(word))
+        self.word = word.lower()
+        self.show_only = show_only
+        if not show_only:
+            self.show_only = set()
+
+    def __len__(self):
+        return len(self.word)
+
+    def __contains__(self, letter: str) -> bool:
+        """ Checks whether the given letter is within the word without taking capitalization into account
+
+        For example:
+        "a" in action == True
+        "A" in action == True
+        "p" in action == False
+
+        :param letter - the letter to check
+        :returns True if the letter is in the word, False otherwise
+        """
+        return letter.lower() in self.word
+
+    def __repr__(self) -> str:
+        """ When repr() is called on the string, exclude characters not in the self.show_only set if it's nonempty
+
+        :returns either the string as is or the string with excluded
+        """
+        return_str = self.word
+        if self.show_only:
+            return_str = ""
+            # Only show characters that are in the given show_only set
+            for char in self.word:
+                if char.lower() in self.show_only or char.upper() in self.show_only:
+                    return_str += char
+                else:
+                    return_str += "_"
+        return return_str
+
+    def __str__(self) -> str:
+        return self.__repr__()
+
+    def __setattr__(self, key: str, value: any) -> None:
+        """ Overrides the set attribute functionality to always take the lowercase of a word
+
+        For example:
+        my_word = Word("hello")
+        my_word.word = "Hi"
+        my_word.word == "hi" # True
+
+        :param key - the object attribute to set
+        :param value - the value to assign to the attribute
+        """
+        # Any attribute other than word will be set normally and word will always be taken as lowercase
+        self.__dict__[key] = value
+        if key == "word":
+            self.__dict__[key] = value.lower()
+
+
+class Dictionary(object):
+    """ Represents a dictionary to get words from for word games
+
+    Constructor arguments:
+
+    :param config - a configuration dictionary containing the file packaged up with pygme containing all words
+    """
     def __init__(self, config: dict) -> None:
         self.dictionary_filename = config["dictionary_filename"]
         self.words = []
@@ -17,7 +93,7 @@ class Dictionary(object):
         full_path = os.path.join(directory_path, self.dictionary_filename)
         with open(full_path, "r") as f:
             words = f.readlines()
-        self.words = [word.strip("\n") for word in words]
+        self.words = [Word(word.strip("\n")) for word in words]
 
     def get_random_word(self, min_length: int = 1, max_length: int = sys.maxsize) -> str:
         """ Retrieves a random word with length between the given minimum and max length arguments
--- test/utils/test_dictionary.py
+++ test/utils/test_dictionary.py
@@ -4,6 +4,41 @@ import random
 from pygme.utils import dictionary
 
 
+def test_word_assignment():
+    """ Tests __setattr__ method of utils.dictionary.Word class """
+    # The constructor will always convert the given word to lowercase
+    word_object = dictionary.Word("Hello")
+    assert word_object.word == "hello"
+    # Word attribute should always be lower case
+    word_object.word = "Hi"
+    assert word_object.word == "hi"
+    # Other attributes will be assigned normally
+    word_object.some_other_attr = "Yo"
+    assert word_object.some_other_attr == "Yo"
+
+
+def test_word_contains():
+    """ Tests __contains__ method of utils.dictionary.Word class """
+    word_object = dictionary.Word("Hello")
+    assert "l" in word_object and "L" in word_object
+    assert "n" not in word_object and "N" not in word_object
+
+
+def test_word_representation():
+    """ Tests repr and str methods of utils.dictionary.Word class """
+    word_object = dictionary.Word("Hello")
+    assert repr(word_object) == str(word_object) and repr(word_object) == "hello"
+    word_object = dictionary.Word("hello", show_only={"E", "o"})
+    assert repr(word_object) == str(word_object) and repr(word_object) == "_e__o"
+
+
+def test_word_length():
+    """ Tests __len__ method of utils.dictionary.Word class """
+    for word in ["Hello", "hi", "testing", "a"]:
+        word_object = dictionary.Word(word)
+        assert len(word_object) == len(word)
+
+
 def test_load_dictionary():
     """ Tests _load_dictionary method of utils.dictionary.Dictionary class """
     dictionary_object = dictionary.Dictionary(pytest.default_dictionary_config)
